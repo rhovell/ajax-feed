f8d99c0a69ae157044a16f8ab4401447e1c186da
Rachel Hovell <r.hovell@live.com>
2018-08-03 16:54:15 +0100

    Initial commit
    All 3 complete apps

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -1,5 +1,3 @@
-/* eslint-env jquery */
-
 (function () {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
@@ -10,5 +8,55 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+        $.ajax({
+            url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+            headers: {
+                Authorization: 'Client-ID 462d22cae6dd1d4877bb082c9e9c6502893a9bb7305d4bf8f681d13b56d4abc3'
+            }
+        }).done(addImage);
+
+        $.ajax({
+            url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=e6a9801dab184d89a4d77b94ff44048c`
+        }).done(addArticles);
+    //     const articleRequest = new XMLHttpRequest();
+    //     articleRequest.onload = addArticles;
+    //     articleRequest.onerror = function (err) {
+    //         requestError(err, 'articles');
+    //     };
+    //     articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=e6a9801dab184d89a4d77b94ff44048c`);
+    //     articleRequest.send();
     });
+
+    function addImage(images) {
+        const firstImage = images.results[0];
+
+        responseContainer.insertAdjacentHTML('afterbegin', `<figure>
+                <img src="${firstImage.urls.small}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`
+        );
+    }
+
+    function addArticles(data) {
+        let htmlContent = '';
+        // const data = JSON.parse(this.responseText);
+        // const firstArticle = article.results[0];
+        if (data.response && data.response.docs && data.response.docs.length > 1) {
+            const articles = data.response.docs;
+            htmlContent = '<ul>' + articles.map(article => `<li class="article">
+                    <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                    <p>${article.snippet}</p>
+                </li>`
+            ).join('') + '</ul>';
+        } else {
+            htmlContent = '<div class="error-no-articles">No articles available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+    //
+    // function requestError(e, part) {
+    //     console.log(e);
+    //     responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning error-${part}">Oh no! There was an error making a request for the ${part}.</p>`);
+    // }
 })();
--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -8,5 +8,63 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+        // fetch the unsplash image content
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+            headers: {
+                Authorization: 'Client-ID 462d22cae6dd1d4877bb082c9e9c6502893a9bb7305d4bf8f681d13b56d4abc3'
+            }
+        })
+            // then parse the information though .json()
+            .then(response => response.json())
+            // then add the image to the page
+            .then(addImage)
+            // catch if search or network error
+            .catch(e => requestError(e, 'image'));
+        // add images to the page function
+        function addImage(data) {
+            let htmlContent = '';
+            const firstImage = data.results[0];
+            // if image data exists, add it to the page
+            if (firstImage) {
+                htmlContent = `<figure>
+                  <img src="${firstImage.urls.small}" alt="${searchedForText}">
+                  <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+              </figure>`;
+                // if the data doesn't exist, display below content
+            } else {
+                htmlContent = 'Unfortunately, no image was returned for your search.';
+            }
+            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        }
+        // error thrown upon network error or search error
+        function requestError(e, part) {
+            console.log(e);
+            responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Oh no! There was an error making a request for the ${part}.</p>`);
+        }
+        // fetch the nytimes article
+        fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=e6a9801dab184d89a4d77b94ff44048c`)
+            // then parse the result
+            .then(response => response.json())
+            // then add the article to the page
+            .then(addArticle)
+            // catch upon network or search error
+            .catch(e => requestError(e, 'article'));
+        // add articles to page
+        function addArticle(data) {
+            let htmlContent = '';
+            // if article exists add it to the page
+            if (data.response && data.response.docs && data.response.docs.length > 1) {
+                const articles = data.response.docs;
+                htmlContent = '<ul>' + articles.map(article => `<li class="article">
+                      <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                            <p>${article.snippet}</p>
+                  </li>`
+                ).join('') + '</ul>';
+                // if article doesn't exist then add the below to the page
+            } else {
+                htmlContent = '<div class="error-no-articles">No articles available</div>';
+            }
+            responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+        }
     });
 })();
